5937bf9ae6ec12de7ddcde1ed542e8f01c164b18
Liney <lineyhamilton@gmail.com>
2015-08-17 21:45:15 -0700

    Adding extra week four lecture material

--- Week Four/script.js
+++ Week Four/script.js
@@ -10,3 +10,15 @@ console.log(sum);
 var isItfifteen = confirm("Is the sum 15?");
 
 sum = prompt("What is the sum?"); */
+
+var age = prompt("How old are you?");
+
+age = parseInt(age);
+
+if (isNaN(age)){
+	alert("BONED!!");
+} else if (age < 200){
+	alert("So young!!");
+} else {
+	alert("Methusela");
+}
